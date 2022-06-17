27fd6604258905e121088be4e882df0fe5226821
SlawaSlawaSlawa <100555254+SlawaSlawaSlawa@users.noreply.github.com>
2022-06-17 21:33:13 +0400

    day: 2

---
+++ scripts/scrollLoad.js
@@ -0,0 +1,26 @@
+import { getData } from './getData.js'
+import { createCardPhoto } from './createCardPhoto.js'
+
+
+export const scrollLoad = (gallery, grid, endElem) => {
+    const observer = new IntersectionObserver(
+        async (entries) => {
+            if (entries[0].isIntersecting) {
+                const photos = await getData()
+                const cards = photos.map(photo => {
+                    return createCardPhoto(photo)
+                })
+
+                Promise.all(cards)
+                .then(cards => {
+                    gallery.append(...cards)
+                    grid.appended(cards)
+                })
+            }
+        }, {
+            rootMargin: '150px'
+        }
+    )
+
+    observer.observe(endElem)
+}
